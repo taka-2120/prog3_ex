841508d84253fef08a598523c4146eb0cc52c88a
taka-2120 <yu_tkhs@icloud.com>
2023-10-27 00:55:37 +0900

    fix: 01_main.c

--- t1/01/01_main.c
+++ t1/01/01_main.c
@@ -5,10 +5,10 @@ int main()
 {
   int a = 10;
   int b = 20;
-  int *min, *max;
+  int min = 0, max = 0;
 
   // min_max関数を呼び出す
-  min_max(min, max, a, b);
+  min_max(&min, &max, a, b);
 
   // 結果を出力する
   printf("min = %d, max = %d\n", min, max);
